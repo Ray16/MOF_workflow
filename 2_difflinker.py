e10c3c63153b9b3d7e218bad633d4a4781397050
Ruijie Zhu <ray19970727@gmail.com>
2023-02-10 17:32:42 -0600

    change code

--- 2_difflinker.py
+++ 2_difflinker.py
@@ -4,5 +4,7 @@ from subprocess import run
 nodes = [i.split('_')[1].split('.sdf')[0] for i in os.listdir('data') if 'conformers' in i]
 
 for node in nodes:
-    run(['python','-W','ignore','difflinker_sample_and_analyze.py','--fragments',f'data/fragments_all/{node}/hMOF_frag.sdf','--model','models/geom_difflinker.ckpt','--linker_size','models/geom_size_gnn.ckpt','--output','output','--samples_dir','output_samples','--n_samples','1'])
+    OUTPUT_SAMPLE_DIR = f'output_samples/{node}'
+    os.mkdir(OUTPUT_SAMPLE_DIR)
+    run(['python','-W','ignore','difflinker_sample_and_analyze.py','--fragments',f'data/fragments_all/{node}/hMOF_frag.sdf','--model','models/geom_difflinker.ckpt','--linker_size','models/geom_size_gnn.ckpt','--output','output','--samples_dir',OUTPUT_SAMPLE_DIR,'--n_samples','1'])
     break
