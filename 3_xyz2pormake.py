fe35e4b017681a92c7afada8c6020f3d47b36ec0
Ruijie Zhu <ray19970727@gmail.com>
2023-02-13 17:56:40 -0600

    update

--- 3_xyz2pormake.py
+++ 3_xyz2pormake.py
@@ -13,18 +13,21 @@ for node in nodes:
     base_dir = f'output/{node}/'
     for file in os.listdir(base_dir):
         if not file.startswith('.'):
-            mol_num = file.split('_')[1]
-            # generate smile strings
-            result = subprocess.run(f'obabel {os.path.join(base_dir,file)} -osmi', shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True).stdout.split()[0]
-            # remove square brackets
-            result = result.replace('[','').replace(']','')
-            os.makedirs(f'output_for_pormake/xyz_h/{node}/',exist_ok=True)
-            os.makedirs(f'output_for_pormake/xyz_X/{node}/',exist_ok=True)
-            # add hydrogen
-            target_xyz_path = f'output_for_pormake/xyz_h/{node}/mol_{mol_num}.xyz'
-            subprocess.run(f'obabel -:"{result}" --gen3D -O {target_xyz_path}', shell=True)
-            # remove invalid structures
-            info = ''.join(open(target_xyz_path).readlines())
-            if 'nan' in info:
-                os.remove(target_xyz_path)
+            try:
+                mol_num = file.split('_')[1]
+                # generate smile strings
+                result = subprocess.run(f'obabel {os.path.join(base_dir,file)} -osmi', shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True).stdout.split()[0]
+                # remove square brackets
+                result = result.replace('[','').replace(']','')
+                os.makedirs(f'output_for_pormake/xyz_h/{node}/',exist_ok=True)
+                os.makedirs(f'output_for_pormake/xyz_X/{node}/',exist_ok=True)
+                # add hydrogen
+                target_xyz_path = f'output_for_pormake/xyz_h/{node}/mol_{mol_num}.xyz'
+                subprocess.run(f'obabel -:"{result}" --gen3D -O {target_xyz_path}', shell=True)
+                # remove invalid structures
+                info = ''.join(open(target_xyz_path).readlines())
+                if 'nan' in info:
+                    os.remove(target_xyz_path)
+            except:
+                pass
 
