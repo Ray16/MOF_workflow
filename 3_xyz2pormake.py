6781be7706e9e1728ae21ef10caa850bd2d5dc79
Ruijie Zhu <ray19970727@gmail.com>
2023-02-13 17:51:23 -0600

    updated file path

--- 3_xyz2pormake.py
+++ 3_xyz2pormake.py
@@ -6,6 +6,8 @@ from subprocess import PIPE
 
 nodes = [i.split('_')[1].split('.sdf')[0] for i in os.listdir('data/conformers') if 'conformers' in i]
 
+os.mkdir('output_for_pormake')
+
 for node in nodes:
     base_dir = f'output/{node}/'
     for file in os.listdir(base_dir):
@@ -15,8 +17,10 @@ for node in nodes:
             result = subprocess.run(f'obabel {os.path.join(base_dir,file)} -osmi', shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True).stdout.split()[0]
             # remove square brackets
             result = result.replace('[','').replace(']','')
+            os.mkdir('output_for_pormake/xyz_h')
+            os.mkdir('output_for_pormake/xyz_X')
             # add hydrogen
-            target_xyz_path = f'xyz_h/mol_{mol_num}.xyz'
+            target_xyz_path = f'output_for_pormake/xyz_h/mol_{mol_num}.xyz'
             subprocess.run(f'obabel -:"{result}" --gen3D -O {target_xyz_path}', shell=True)
             # remove invalid structures
             info = ''.join(open(target_xyz_path).readlines())
