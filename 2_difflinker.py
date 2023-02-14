e4c0c0cf0a264dadf1d8e2512d0d20be3384984b
Ruijie Zhu <ray19970727@gmail.com>
2023-02-14 16:55:17 -0600

    update

--- 2_difflinker.py
+++ 2_difflinker.py
@@ -1,12 +1,12 @@
 import os
-from tqdm import tqdm
 import subprocess
 
 nodes = [i.split('_')[1].split('.sdf')[0] for i in os.listdir('data/conformers') if 'conformers' in i]
 
 os.makedirs('output',exist_ok=True)
 
-for node in tqdm(nodes):
+for node in nodes:
+    print(f'Now on node: {node}')
     OUTPUT_DIR = f'output/{node}'
     os.makedirs(OUTPUT_DIR,exist_ok=True)
     subprocess.run(f'python -W ignore difflinker_sample_and_analyze.py --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --linker_size models/geom_size_gnn.ckpt --output {OUTPUT_DIR} --n_samples 1',shell=True)
