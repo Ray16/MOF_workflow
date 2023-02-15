1e9e964ffe1f8d2b0fe7a47caab7a0ffe5f99d7b
Ruijie Zhu <ray19970727@gmail.com>
2023-02-15 17:41:34 -0600

    test

--- 4_assemble.py
+++ 4_assemble.py
@@ -1,41 +1,36 @@
 import os
 import sys
-import re
-import ast
-import json
-import random
 import pathlib
 import shutil
 
-from tqdm import tqdm
 import timeout_decorator
-import pormake as pm
 
 os.makedirs('MOFs',exist_ok=True)
 
 linkers_dir = 'output_for_pormake/xyz_X'
 nodes = [i.split('.')[0] for i in os.listdir(linkers_dir)]
 
-database = pm.Database()
 #all_tpos = [i.split('.')[0] for i in os.listdir(tpo_dir)]
 #all_sbu = [i.split('.')[0] for i in os.listdir(sbu_dir) if not i.startswith('.')]
 #all_linkers = sorted([i for i in all_sbu if i.startswith('E')])
 #all_nodes = sorted([i for i in all_sbu if not i.startswith('E')])
 
-
 @timeout_decorator.timeout(5)
 def gen_mof(node,linker,tpo):
+    sys.path.insert(1, os.path.join('MOFs',node))
+    import pormake as pm
+    builder = pm.Builder()
+    database = pm.Database()
+    
     T = database.get_topo(tpo)
     N = database.get_bb(node)
     L = database.get_bb(linker)
-
-    builder = pm.Builder()
 
     node_bbs = {0: N}
 
     edge_bbs = {(0, 0): L}
     if node+'_'+linker+'.cif' not in os.listdir(mof_dir):
-            MOF = builder.build_by_type(topology=T, node_bbs=node_bbs, edge_bbs = edge_bbs)
+            MOF = builder.build_by_type(topology=T, node_bbs=node_bbs, edge_bbs=edge_bbs)
             cif_name = tpo+'_'+node+'_'+linker+'.cif'
             MOF.write_cif(mof_dir+cif_name)
 
