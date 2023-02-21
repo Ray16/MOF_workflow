b9f5814fa2cb6267d0ea6efcc521e6c2cd00d2c9
Ruijie Zhu <ray19970727@gmail.com>
2023-02-21 16:59:38 -0600

    debug

--- 4_assemble.py
+++ 4_assemble.py
@@ -38,36 +38,52 @@ if __name__ == '__main__':
     os.makedirs('MOFs',exist_ok=True)
     node_dir = 'node_xyz'
 
+    # append pormake path to sys
+    sys.path.append('PORMAKE_template')
+    print('Loading PORMAKE ...')
+    import pormake as pm
+    print('Finished loading PORMAKE ...')
+
     for node in nodes:
         if node != 'V':
             print(f'Now generating MOFs with node: {node}')
+            pormake_dir = 'PORMAKE_template'
             target_mof_dir = os.path.join('MOFs',node)
-            mof_dir = f'MOFs/{node}/gen_mofs'
+            mof_dir = f'MOFs/{node}/'
+            os.makedirs(mof_dir,exist_ok=True)
 
             # copy template PORMAKE code to MOFs dir
-            shutil.copytree('PORMAKE_template',target_mof_dir,dirs_exist_ok=True)
+            #shutil.copytree('PORMAKE_template',target_mof_dir,dirs_exist_ok=True)
 
-            # copy node to bbs dir
+            # copy node to template pormake bbs dir
             print(f'Copying node {node} to pormake dir ...')
-            shutil.copy(os.path.join(node_dir,node+'.xyz'),os.path.join(target_mof_dir,'pormake','database','bbs'))
+            shutil.copy(os.path.join(node_dir,node+'.xyz'),os.path.join(pormake_dir,'pormake','database','bbs'))
 
-            # copy linkers to bbs dir
+            # copy linkers to template poramke bbs dir
             print('Copying linkers to pormake dir ...')
             for linker in os.listdir(os.path.join(linkers_dir,node)):
-                shutil.copy(os.path.join(linkers_dir,node,linker),os.path.join(target_mof_dir,'pormake','database','bbs'))
+                shutil.copy(os.path.join(linkers_dir,node,linker),os.path.join(pormake_dir,'pormake','database','bbs'))
 
             # append pormake path
-            sys.path.append(os.path.join('MOFs',node)) # append pormake path to sys
-            os.chdir(os.path.join(parent_dir,'MOFs',node))
-            import pormake as pm
+            #sys.path.append(os.path.join('MOFs',node)) # append pormake path to sys
+            #os.chdir(os.path.join(parent_dir,'MOFs',node))
 
             # generate MOF
             linker_names = [i.split('.')[0] for i in os.listdir(os.path.join(linkers_dir,node)) if 'E_' in i]
             for l in tqdm(linker_names):
                 gen_mof(node,l,'pcu')
                 break
-            del pm
-            os.chdir(parent_dir)
+            
+            # move generated MOFs to the target dir
+            #for MOF in os.path.join():
+            #    shutil.move('',)
+
+            # remove sbus from PORMAKE template folder
+            shutil.rmtree(os.path.join(pormake_dir,'pormake','database','bbs'))
+            os.mkdir(os.path.join(pormake_dir,'pormake','database','bbs'))
+
+            #del pm
+            #os.chdir(parent_dir)
             # remove completed pormake job path from sys path
-            sys.path.remove(os.path.join('MOFs',node))
-            sys.modules.pop('pormake')
+            #sys.path.remove(os.path.join('MOFs',node))
+            #sys.modules.pop('pormake')
