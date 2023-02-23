400fadf9ab89e1a519c476740a1e3ee35838a607
Ruijie Zhu <ray19970727@gmail.com>
2023-02-22 22:17:26 -0600

    test

--- 5_output_metrics.py
+++ 5_output_metrics.py
@@ -3,7 +3,7 @@ import pandas as pd
 from rdkit import Chem
 
 true_base_dir = 'data/fragments_all/'
-pred_base_dir = 'output_for_pormake/xyz_h/'
+pred_base_dir = 'output/'
 
 os.makedirs('metrics',exist_ok=True)
 for sys in os.listdir(pred_base_dir):
@@ -17,7 +17,7 @@ for sys in os.listdir(pred_base_dir):
         # generate for SMILES_frag
         frag_smiles_all.append(line[2])
         # generate for SMILES_pred
-        pred_mol = Chem.MolFromXYZBlock(os.path.join(pred_base_dir,sys,linker))
+        pred_mol = open(os.path.join(pred_base_dir,'smiles_'+sys+'.csv')).readlines()[i]
         pred_smiles_all.append(Chem.MolToSmiles(pred_mol))
     print(true_smiles_all)
     print(frag_smiles_all)
