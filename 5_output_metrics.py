d50841e0cfac8277d8b41034c8e4eb1f996f0f37
Ruijie Zhu <ray19970727@gmail.com>
2023-02-22 22:38:17 -0600

    debug

--- 5_output_metrics.py
+++ 5_output_metrics.py
@@ -20,7 +20,7 @@ for sys in os.listdir(pred_base_dir):
             frag_smiles_all.append(line[2].strip())
             # generate for SMILES_pred
             pred_smiles_all.append(open(os.path.join(pred_base_dir,'smiles_'+sys+'.csv')).readlines()[i])
-        df = pd.DataFrame({'true_molecules':true_smiles_all,'pred_molecules':pred_smiles_all,'frag_molecules':frag_smiles_all})
+        df = pd.DataFrame({'index':range(len(true_smiles_all)),'true_molecules':true_smiles_all,'pred_molecules':pred_smiles_all,'frag_molecules':frag_smiles_all})
         df = df[~df["pred_molecules"].str.contains('@')] # remove bad entries
         df.to_csv(f'metrics/{sys}.csv',index=False)
     subprocess.run([f'python -m evaluation.linkers --save_result --filename metrics/{sys}.csv'])
