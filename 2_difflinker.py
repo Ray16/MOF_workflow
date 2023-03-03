447c3c88b3f6c1afc59a3658ac5148f270ca2f29
Ruijie Zhu <ray19970727@gmail.com>
2023-03-03 07:28:21 -0500

    added func to vary n_atom

--- 2_difflinker.py
+++ 2_difflinker.py
@@ -1,19 +1,20 @@
 import os
 import argparse
 import subprocess
-import multiprocessing
+#import multiprocessing
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--n_atoms', action='store', type=int, required=True, help='number of atoms to sample')
-args = parser.parse_args()
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--n_atoms', action='store', type=int, required=True, help='number of atoms to sample')
+#args = parser.parse_args()
 
 nodes = [i.split('_')[1].split('.sdf')[0] for i in os.listdir('data/conformers') if 'conformers' in i]
 
-n_atoms = args.n_atoms
-print(f'Sampling {n_atoms} atoms ...')
-for node in nodes:
-    print(f'Now on node: {node}')
-    OUTPUT_DIR = f'output/n_atoms_{n_atoms}/{node}'
-    os.makedirs(OUTPUT_DIR,exist_ok=True)
-    subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size {n_atoms} --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 1',shell=True)
-    #subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size models/geom_size_gnn.ckpt --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 1',shell=True) 
+#n_atoms = args.n_atoms
+for n_atoms in range(5,10):
+    print(f'Sampling {n_atoms} atoms ...')
+    for node in nodes:
+        print(f'Now on node: {node}')
+        OUTPUT_DIR = f'output/n_atoms_{n_atoms}/{node}'
+        os.makedirs(OUTPUT_DIR,exist_ok=True)
+        subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size {n_atoms} --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 1',shell=True)
+        #subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size models/geom_size_gnn.ckpt --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 1',shell=True) 
